83d2b64dc7eb3a7be4bead1891a4153abf9179ba
Jessica Bunyan <jessicabunyan93@gmail.com>
2024-03-03 17:03:13 +0000

    rudimentary help screen

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { useCallback, useRef, useState } from "react";
 import randElement from "./utils/randElement";
 import pokemon from "./GameFiles/pokemon";
 import Game from "./Components/Game";
+import ReactModal from "react-modal";
 
 function App() {
 	const toastPosition = window.innerWidth <= 600 ? "top-center" : "bottom-right";
@@ -22,6 +23,24 @@ function App() {
 		<div className="app">
 			<TopBar onReset={onReset} setShowHelp={setShowHelp} />
 			<div className="game-container">{target && <Game key={target} word={target} validWords={validWords} />}</div>;
+			{showHelp && (
+				<ReactModal isOpen={showHelp}>
+					<h1 style={{ fontSize: "1.5rem" }}>Help v0</h1>
+					<ul>
+						<li>. </li>
+						<li>This is in beta</li>
+						<li>. </li>
+						<li>Only valid pokemon are accepted</li>
+						<li> .</li>
+						<li>Answers can be any length from 3-15 chars</li>
+						<li> .</li>
+						<li>If the diamond goes green your guess was the correct length</li>
+						<li> .</li>
+						<li>Bad luck if you get Mr.Mime or Farfetchd as they are broken due to punctuation</li>
+					</ul>
+					<button onClick={() => setShowHelp(false)}>Close</button>
+				</ReactModal>
+			)}
 			<Toaster
 				toastOptions={{
 					duration: 3000,
